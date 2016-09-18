d3f8f79d52d800b12f67c88a5b771b001d98fa83
Jason Liu <jase11554@yahoo.com>
2016-09-18 18:12:19 -0500

    Initial Commit

---
+++ Multiply/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiply
+//
+//  Created by jliu on 9/18/16.
+//  Copyright © 2016 Jason Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
